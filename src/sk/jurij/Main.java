96e47b9e0f4d7ff64d823180825d352d75759426
gachojuraj <mmamma151@gmail.com>
2020-09-22 22:52:54 +0200

    code cleanup

--- src/sk/jurij/Main.java
+++ src/sk/jurij/Main.java
@@ -1,7 +1,5 @@
 package sk.jurij;
 
-import sk.jurij.excercises.*;
-
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome, all exercises can be found in exercises package!");
--- src/sk/jurij/excercises/Equality.java
+++ src/sk/jurij/excercises/Equality.java
@@ -1,12 +1,11 @@
 package sk.jurij.excercises;
 
 public class Equality {
-
     public static int equal(int a, int b, int c){
         int[] arr = {a,b,c};
-        short count = 0;
-        for (int  i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        byte count = 0;
+        for (byte  i = 0; i < 3; i++)
+            for (byte j = 0; j < 3; j++)
                 if (arr[i] == arr[j] && i != j){
                     count++;
                     break;
